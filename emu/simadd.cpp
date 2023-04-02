a75bd2b1cf28a6370e21a5ca1d48274eacd3c74f
Chen Lu <181250012@smail.nju.edu.cn>
2023-04-02 16:25:43 +0800

    emu: add simadd

---
+++ emu/simadd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <top.h>
+int main() {
+  S_add* mod = new S_add();
+  for (int i = 1; i < 10; i++) {
+    mod->set_io_a(i);
+    mod->set_io_b(i+1);
+    mod->step();
+    std::cout << mod->io_result << std::endl;
+  }
+}
