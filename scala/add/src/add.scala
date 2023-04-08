2b0224983a64c9c6452869f254be08205d743ea9
Chen Lu <181250012@smail.nju.edu.cn>
2023-04-08 11:50:49 +0800

    test: update add

--- scala/add/src/add.scala
+++ scala/add/src/add.scala
@@ -1,11 +1,23 @@
 import chisel3._
 import chisel3.util._
 
-class add extends Module {
+class _add extends Module {
   val io = IO(new Bundle {
     val a = Input(UInt(32.W))
     val b = Input(UInt(32.W))
     val result = Output(UInt(32.W))
   })
-  io.result := io.a + io.b
+  val __sub = Module(new _sub)
+  __sub.io.a := io.a
+  __sub.io.b := io.b
+  io.result := io.a + io.b + __sub.io.result
+}
+
+class _sub extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(32.W))
+    val b = Input(UInt(32.W))
+    val result = Output(UInt(32.W))
+  })
+  io.result := io.a - io.b
 }
