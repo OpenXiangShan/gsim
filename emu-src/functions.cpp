d5cf69038ef8eb1431690fca64bb3f846cc3a2ea
Chen Lu <181250012@smail.nju.edu.cn>
2023-04-17 14:12:41 +0800

    emu-src: add more functions

--- emu-src/functions.cpp
+++ emu-src/functions.cpp
@@ -17,18 +17,31 @@ void s_cat(mpz_t& dst, mpz_t& src1, mpz_t& src2) {
   mpz_mul_2exp(dst, src1, src2_bits);
   mpz_ior(dst, dst, src2); 
 }
+//s_cat_ui
+void s_cat_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_mul_2exp(dst, src, bitcnt);
+  mpz_add_ui(dst, dst, val);
+}
+//
+void s_cat_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val);
+  mpz_mul_2exp(dst, dst, bitcnt2);
+  mpz_add_ui(dst, dst, val);
+}
+void s_cat_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1);
+  mpz_mul_2exp(dst, dst, bitcnt2);
+  mpz_add_ui(dst, dst, val2);
+}
 //s_asSInt
 void s_asSInt(mpz_t& dst, mpz_t& src) {
-  mp_bitcnt_t shiftcnt;
-  assert((shiftcnt = mpz_sizeinbase(src, 2)) <= 64);
-  if(shiftcnt == 0) {
-    mpz_set(dst, src);
-    return;
-  }
-  long data = (long)mpz_get_ui(src);
-  shiftcnt = 64 - shiftcnt;
-  data = (data << shiftcnt) >> shiftcnt;
-  mpz_set_si(dst, data);
+  mpz_set(dst, src);
+  mpz_limbs_finish(dst, -mpz_size(src));
+}
+//s_asUInt
+void s_asUInt(mpz_t& dst, mpz_t& src) {
+  mpz_set(dst, src);
+  mpz_limbs_finish(dst, mpz_size(src));
 }
 //s_bits
 void s_bits(mpz_t& dst, mpz_t& src, mp_bitcnt_t l, mp_bitcnt_t r) {
@@ -52,4 +65,129 @@ void s_pad(mpz_t& dst, mpz_t& src, mp_bitcnt_t n) {
   mpz_sub_ui(dst, dst, 1);
   mpz_mul_2exp(dst, dst, bitcnt);
   mpz_ior(dst, dst, src);
+}
+//add
+void s_mpz_add_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_add_ui(dst, src, val);
+}
+void s_mpz_add_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_add_ui(dst, src, val);
+}
+void s_mpz_add_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1);
+  mpz_add_ui(dst, dst, val2);
+}
+//lt
+void s_mpz_lt(mpz_t& dst, mpz_t& op1, mpz_t& op2) {
+  mpz_set_ui(dst, mpz_cmp(op1, op2)<0);
+}
+void s_mpz_lt_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)<0);
+}
+void s_mpz_lt_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)>0);
+}
+void s_mpz_lt_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 < val2);
+}
+//leq
+void s_mpz_leq(mpz_t& dst, mpz_t& op1, mpz_t& op2) {
+  mpz_set_ui(dst, mpz_cmp(op1, op2)<=0);
+}
+void s_mpz_leq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)<=0);
+}
+void s_mpz_leq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)>=0);
+}
+void s_mpz_leq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 <= val2);
+}
+//gt
+void s_mpz_gt(mpz_t& dst, mpz_t& op1, mpz_t& op2) {
+  mpz_set_ui(dst, mpz_cmp(op1, op2)>0);
+}
+void s_mpz_gt_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)>0);
+}
+void s_mpz_gt_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)<0);
+}
+void s_mpz_gt_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 > val2);
+}
+//geq
+void s_mpz_geq(mpz_t& dst, mpz_t& op1, mpz_t& op2) {
+  mpz_set_ui(dst, mpz_cmp(op1, op2)>=0);
+}
+void s_mpz_geq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)>=0);
+}
+void s_mpz_geq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)<=0);
+}
+void s_mpz_geq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 >= val2);
+}
+//eq
+void s_mpz_eq(mpz_t& dst, mpz_t& op1, mpz_t& op2) {
+  mpz_set_ui(dst, mpz_cmp(op1, op2)==0);
+}
+void s_mpz_eq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)==0);
+}
+void s_mpz_eq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)==0);
+}
+void s_mpz_eq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 == val2);
+}
+//neq
+void s_mpz_neq(mpz_t& dst, mpz_t& op1, mpz_t& op2) {
+  mpz_set_ui(dst, mpz_cmp(op1, op2)!=0);
+}
+void s_mpz_neq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)!=0);
+}
+void s_mpz_neq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, mpz_cmp_ui(src, val)!=0);
+}
+void s_mpz_neq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 != val2);
+}
+//and
+void s_mpz_and_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, val);
+  mpz_and(dst, dst, src);
+}
+void s_mpz_and_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val);
+  mpz_and(dst, dst, src);
+}
+void s_mpz_and_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 & val2);
+}
+//ior
+void s_mpz_ior_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, val);
+  mpz_ior(dst, dst, src);
+}
+void s_mpz_ior_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val);
+  mpz_ior(dst, dst, src);
+}
+void s_mpz_ior_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 | val2);
+}
+//xor
+void s_mpz_xor_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt) {
+  mpz_set_ui(dst, val);
+  mpz_xor(dst, dst, src);
+}
+void s_mpz_xor_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val);
+  mpz_xor(dst, dst, src);
+}
+void s_mpz_xor_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2) {
+  mpz_set_ui(dst, val1 ^ val2);
 }
--- emu-src/functions.h
+++ emu-src/functions.h
@@ -4,7 +4,49 @@
 
 void s_tail(mpz_t& dst, mpz_t& src, unsigned long n);
 void s_cat(mpz_t& dst, mpz_t& src1, mpz_t& src2);
+void s_cat_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_cat_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_cat_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
 void s_asSInt(mpz_t& dst, mpz_t& src);
+void s_asUInt(mpz_t& dst, mpz_t& src);
 void s_bits(mpz_t& dst, mpz_t& src, mp_bitcnt_t l, mp_bitcnt_t r);
 void s_pad(mpz_t& dst, mpz_t& src, mp_bitcnt_t n);
+void s_mpz_add_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_add_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_add_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+//comparison
+void s_mpz_lt(mpz_t& dst, mpz_t& op1, mpz_t& op2);
+void s_mpz_lt_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_lt_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_lt_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+void s_mpz_leq(mpz_t& dst, mpz_t& op1, mpz_t& op2);
+void s_mpz_leq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_leq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_leq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+void s_mpz_gt(mpz_t& dst, mpz_t& op1, mpz_t& op2);
+void s_mpz_gt_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_gt_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_gt_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+void s_mpz_geq(mpz_t& dst, mpz_t& op1, mpz_t& op2);
+void s_mpz_geq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_geq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_geq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+void s_mpz_eq(mpz_t& dst, mpz_t& op1, mpz_t& op2);
+void s_mpz_eq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_eq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_eq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+void s_mpz_neq(mpz_t& dst, mpz_t& op1, mpz_t& op2);
+void s_mpz_neq_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_neq_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_neq_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+//logical
+void s_mpz_and_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_and_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_and_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+void s_mpz_ior_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_ior_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_ior_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
+void s_mpz_xor_ui_r(mpz_t& dst, mpz_t& src, unsigned long val, mp_bitcnt_t bitcnt);
+void s_mpz_xor_ui_l(mpz_t& dst, unsigned long val, mp_bitcnt_t bitcnt1, mpz_t& src, mp_bitcnt_t bitcnt2);
+void s_mpz_xor_ui2(mpz_t& dst, unsigned long val1, mp_bitcnt_t bitcnt1, unsigned long val2, mp_bitcnt_t bitcnt2);
 #endif
