4b3fd324d1a966799cb318dcd77649e2bf4695a1
Chen Lu <181250012@smail.nju.edu.cn>
2023-05-11 20:37:49 +0800

    sigFilter: remove sigs with width > 64

--- scripts/sigFilter.py
+++ scripts/sigFilter.py
@@ -6,6 +6,15 @@ class SigFilter():
     self.reffp = None
     self.dstfp = None
 
+  def width(self, idx, data):
+    endIdx = idx
+    while data[endIdx] != ':':
+      endIdx -= 1
+    startIdx = endIdx
+    while data[startIdx-1] != '*':
+      startIdx -= 1
+    return int(data[startIdx : endIdx]) + 1
+
   def filter(self, srcFile, refFile, dstFile):
     self.srcfp = open(srcFile, "r")
     self.reffp = open(refFile, "r")
@@ -15,7 +24,10 @@ class SigFilter():
     for line in self.srcfp.readlines():
       line = line.strip("\n")
       line = line.split(" ")
-      if ref.find(line[1] + ";") != -1:
+      idx = ref.find(line[1] + ";")
+      if idx != -1:
+        if self.width(idx, ref) > 64:
+          continue
         self.dstfp.writelines( \
         "if(display || mpz_cmp_ui(mod->" + line[0] + ", ref->rootp->" + line[1] + ") != 0){\n" + \
         "  ret = true;\n" + \
