68c3a9899d70dce470fa67b2c0c1d7786fb25f71
Chen Lu <181250012@smail.nju.edu.cn>
2024-04-30 14:31:50 +0800

    defbug,cppEmitter: display reader_data

--- src/cppEmitter.cpp
+++ src/cppEmitter.cpp
@@ -509,6 +509,11 @@ void graph::genMemRead(FILE* fp) {
             fprintf(fp, "memcpy(%s, %s[%s], sizeof(%s));\n", port->member[READER_DATA]->name.c_str(), mem->name.c_str(), port->member[READER_ADDR]->computeInfo->valStr.c_str(), port->member[READER_DATA]->name.c_str());
           else {
             fprintf(fp, "%s = %s[%s];\n", port->member[READER_DATA]->name.c_str(), mem->name.c_str(), port->member[READER_ADDR]->computeInfo->valStr.c_str());
+#ifdef EMU_LOG
+            fprintf(fp, "if (cycles >= %d && cycles <= %d) {\n", LOG_START, LOG_END);
+            fprintf(fp, "printf(\"%%ld read %s %%lx\\n\", cycles, (uint64_t(%s)));\n", port->member[READER_DATA]->name.c_str(), port->member[READER_DATA]->name.c_str());
+            fprintf(fp, "}\n");
+#endif
           }
           activateUncondNext(fp, port->member[READER_DATA], port->member[READER_DATA]->nextActiveId, false);
         }
