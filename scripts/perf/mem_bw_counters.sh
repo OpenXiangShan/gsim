42df68883e94972c06f0504a0c665351a2a1d9ab
Zhou Yaoyang <zhouyaoyang17@mails.ucas.edu.cn>
2024-09-26 17:08:25 +0800

    update performance counters

--- scripts/perf/mem_bw_counters.sh
+++ scripts/perf/mem_bw_counters.sh
@@ -1,15 +1,33 @@
-# full raw event list for memory for AMD 19h model 61h (EPYC 0094), which partially applies to 7950X3D
-# events="r740FE1F,r740FE5F,r740FE9F,r740FE9F,r10740FE1F,r10740FE5F,r10740FE9F,r10740FE9F,r20740FE1F,r20740FE5F,r20740FE9F,r20740FE9F"  # memory read bw
-# events=${events}",r740FF1F,r740FF5F,r740FF9F,r740FF9F,r10740FF1F,r10740FF5F,r10740FF9F,r10740FF9F,r20740FF1F,r20740FF5F,r20740FF9F,r20740FF9F"  # memory write bw
+# From event list for memory for AMD 19h model 61h (EPYC 9004), which partially applies to 7950X3D
 
 events="instructions,cycles"
 
 # events=${events},"r740FE5F"  # memory read bw, where bit 22 is not supported
 events=${events}",r700FE5F"  # memory read bw
 events=${events}",r700FF5F"  # memory write bw
+# events=${events}",r700FE1F,r700FE5F,r700FE9F,r700FE9F,r10700FE1F,r10700FE5F,r10700FE9F,r10700FE9F,r20700FE1F,r20700FE5F,r20700FE9F,r20700FE9F"  # memory read bw
+# events=${events}",r700FF1F,r700FF5F,r700FF9F,r700FF9F,r10700FF1F,r10700FF5F,r10700FF9F,r10700FF9F,r20700FF1F,r20700FF5F,r20700FF9F,r20700FF9F"  # memory write bw
 
-# We expect memory read = L3 (demand) misses + prefetches missed in L3, for double check
-events=${events}",r000104"  # L3 misses
-events=${events}",r00FF71,r00FF72"  # L1D + L2 prefetches missed in L3
+# events=${events}",r00FF71,r00FF72"  # L1D + L2 prefetches missed in L3
+# events=${events}",r000104"  # L3 misses; IDK whether it works, because addr disclosed in offical doc contains some addr not supported
+events=${events}",r000864"  # L2 data misses
+events=${events}",r000164"  # L2 instr misses
+# L2 refill
+events=${events}",r000165"  # L2 fill From local CCX L3
+events=${events}",r000265"  # L2 fill From another CCX L3
+events=${events}",r000465"  # L2 fill From local numa node's dram, aka. demand L3 miss? not sure
+events=${events}",r000865"  # L2 fill From another CCX L3, target another numa node; ???? why not zero on 7950x3d?
+# events=${events}",r001065"  # Reserved; not zero on zen4
+# events=${events}",r002065"  # 
+# events=${events}",r004065"  # 
+# events=${events}",r008065"  # 
 
-
+# L1 Data Cache
+# events=${events}",r000143"  # 
+# events=${events}",r000243"  # 
+# events=${events}",r000443"  # 
+# events=${events}",r000843"  # 
+# events=${events}",r001043"  # 
+# events=${events}",r002043"  # 
+# events=${events}",r004043"  # 
+# events=${events}",r008043"  # 
