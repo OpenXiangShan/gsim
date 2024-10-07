825d3d55d721b4eaed80c6aa3509b578fed45400
Chen Lu <181250012@smail.nju.edu.cn>
2024-10-08 00:22:07 +0800

    insts: recompute when memberInfo.size() not match

--- src/instsGenerator.cpp
+++ src/instsGenerator.cpp
@@ -1894,14 +1894,18 @@ void Node::recompute() {
   if (prevVal->valStr != computeInfo->valStr) {
     recomputeNext = true;
   }
-  for (size_t i = 0; i < prevArrayVal.size(); i ++) {
-    if ((!prevArrayVal[i] && computeInfo->memberInfo[i]) || (prevArrayVal[i] && !computeInfo->memberInfo[i])) {
-      recomputeNext = true;
-      break;
-    }
-    if (prevArrayVal[i] && computeInfo->memberInfo[i] && prevArrayVal[i]->valStr != computeInfo->memberInfo[i]->valStr) {
-      recomputeNext = true;
-      break;
+  if (prevArrayVal.size() != computeInfo->memberInfo.size()) {
+    recomputeNext = true;
+  } else {
+    for (size_t i = 0; i < prevArrayVal.size(); i ++) {
+      if ((!prevArrayVal[i] && computeInfo->memberInfo[i]) || (prevArrayVal[i] && !computeInfo->memberInfo[i])) {
+        recomputeNext = true;
+        break;
+      }
+      if (prevArrayVal[i] && computeInfo->memberInfo[i] && prevArrayVal[i]->valStr != computeInfo->memberInfo[i]->valStr) {
+        recomputeNext = true;
+        break;
+      }
     }
   }
   if (recomputeNext) {
