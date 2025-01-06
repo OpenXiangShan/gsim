a8a955ff663336a9774ab4d9c39f6456b5d852fc
Zihao Yu <yuzihao@ict.ac.cn>
2025-01-06 14:11:37 +0800

    firParser: use async to create threads

--- src/firParser.cpp
+++ src/firParser.cpp
@@ -1,7 +1,24 @@
 #include "common.h"
 #include "syntax.hh"
 #include "Parser.h"
-#include <thread>
+#include <future>
+
+static PList* parseFunc(char *strbuf, PNode **globalRoot, int lineno) {
+  std::istringstream *streamBuf = new std::istringstream(strbuf);
+  Parser::Lexical *lexical = new Parser::Lexical(*streamBuf, std::cout);
+  Parser::Syntax *syntax = new Parser::Syntax(lexical);
+  lexical->set_lineno(lineno);
+  syntax->parse();
+  PList *list = lexical->list;
+  if (globalRoot) {
+    assert(lexical->root != NULL);
+    *globalRoot = lexical->root;
+  }
+  delete syntax;
+  delete lexical;
+  delete streamBuf;
+  return list;
+}
 
 PNode* parseFIR(char *strbuf) {
   char *p = strbuf;
@@ -15,30 +32,14 @@ PNode* parseFIR(char *strbuf) {
   for (char *q = strbuf; (q = strchr(q, '\n')) != NULL; next_lineno ++, q ++);
   next_lineno ++; // '\0' is overwritten originally from '\n', so count it
 
-  std::istringstream *streamBuf = new std::istringstream(strbuf);
-  Parser::Lexical *lexical = new Parser::Lexical(*streamBuf, std::cout);
-  Parser::Syntax *syntax = new Parser::Syntax(lexical);
+  PNode* globalRoot = NULL;
+  std::future<PList *> result[2];
+  result[0] = async(std::launch::async, parseFunc, strbuf, &globalRoot, 1);
+  result[1] = async(std::launch::async, parseFunc, p, nullptr, next_lineno);
+  PList *list0 = result[0].get();
+  PList *list1 = result[1].get();
 
-  std::istringstream *streamBuf2 = new std::istringstream(p);
-  Parser::Lexical *lexical2 = new Parser::Lexical(*streamBuf2, std::cout);
-  Parser::Syntax *syntax2 = new Parser::Syntax(lexical2);
-  lexical2->set_lineno(next_lineno);
-
-  std::thread a([&syntax]  { printf("start A\n"); syntax ->parse(); printf("finish A\n"); }),
-              b([&syntax2] { printf("start B\n"); syntax2->parse(); printf("finish B\n"); });
-  a.join();
-  b.join();
-
-  PNode* globalRoot = lexical->root;
-  lexical->list->concat(lexical2->list);
-  globalRoot->child.assign(lexical->list->siblings.begin(), lexical->list->siblings.end());
-
-  delete syntax;
-  delete lexical;
-  delete streamBuf;
-  delete syntax2;
-  delete lexical2;
-  delete streamBuf2;
-
+  list0->concat(list1);
+  globalRoot->child.assign(list0->siblings.begin(), list0->siblings.end());
   return globalRoot;
 }
