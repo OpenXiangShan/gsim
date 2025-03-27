5293b138bd72a7a94087db829301934737e58088
CoA <1109673069@qq.com>
2025-03-27 17:09:40 +0800

    traversal: make graph::traversal useable before topoSort
    just like GraphDumper::dump()

--- src/traversal.cpp
+++ src/traversal.cpp
@@ -103,7 +103,11 @@ void ExpTree::display() {
 
 /* traverse graph */
 void graph::traversal() {
-  for (SuperNode* super : sortedSuper) {
+  auto list = sortedSuper;
+  if (list.empty()){
+    list = supersrc;
+  }
+  for (auto super : list) {
     super->display();
   }
 }
