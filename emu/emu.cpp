8391d2acf8f586050a5a7cbafbe67d8781dadac2
Chen Lu <chenlu22z@ict.ac.cn>
2025-07-17 17:15:22 +0800

    emu: add parameter into arguments

--- emu/emu.cpp
+++ emu/emu.cpp
@@ -49,7 +49,7 @@
 #endif
 
 // unused blackbox
-void imsic_csr_top(uint8_t _0, uint8_t _1, uint16_t _2, uint8_t _3, uint8_t _4, uint16_t _5, uint8_t _6, uint8_t _7, uint8_t _8, uint8_t _9, uint8_t _10, uint8_t _11, uint64_t _12, uint8_t& _13, uint64_t& _14, uint8_t& _15, uint8_t& _16, uint32_t& _17, uint32_t& _18, uint32_t& _19) {
+void imsic_csr_top(int EID_VLD_DLY_NUM, int NR_HARTS, int NR_INTP_FILES, int NR_SRC, int XLEN, uint8_t _0, uint8_t _1, uint16_t _2, uint8_t _3, uint8_t _4, uint16_t _5, uint8_t _6, uint8_t _7, uint8_t _8, uint8_t _9, uint8_t _10, uint8_t _11, uint64_t _12, uint8_t& _13, uint64_t& _14, uint8_t& _15, uint8_t& _16, uint32_t& _17, uint32_t& _18, uint32_t& _19) {
   _13 = 0;
   _15 = 0;
   _16 = 0; // o_irq
@@ -59,7 +59,7 @@ void imsic_csr_top(uint8_t _0, uint8_t _1, uint16_t _2, uint8_t _3, uint8_t _4, 
 }
 
 // unused blackbox
-void SimJTAG(uint8_t _0, uint8_t& _1, uint8_t& _2, uint8_t& _3, uint8_t _4, uint8_t _5, uint8_t _6, uint8_t _7, uint32_t& _8) {
+void SimJTAG(int param, uint8_t _0, uint8_t& _1, uint8_t& _2, uint8_t& _3, uint8_t _4, uint8_t _5, uint8_t _6, uint8_t _7, uint32_t& _8) {
   _1 = 1; // TRSTn
   _2 = 0; // TMS
   _3 = 0; // TDI
